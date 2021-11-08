12afe1ea4c73d0efa34ccad59e888594591cec38
AlanJs26 <alanjoses.29@gmail.com>
2021-11-08 17:08:44 -0300

    updated the installation script

--- install.sh
+++ install.sh
@@ -9,6 +9,5 @@ cp -r scrappers "$HOME/Documentos/animedl/scrappers"
 echo "$(which python3) '$HOME/Documentos/animedl/anime.py' \$@" > "$HOME/Documentos/animedl/anime.sh"
 sudo chmod u+x "$HOME/Documentos/animedl/anime.sh"
 sudo ln -sf "$HOME/Documentos/animedl/anime.sh" "$HOME/.local/bin/anime"
-#ln "$HOME/Documentos/animedl/anime.sh" anime
 
-printf "\ngoyabu-cli instalado com sucesso!\n\ndigite 'anime' no terminal para começar"
+printf "\ngoyabu-cli instalado com sucesso!\n\ndigite \033[92m'anime'\033[0m no terminal para começar\n"
